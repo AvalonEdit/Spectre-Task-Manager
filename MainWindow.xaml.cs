e7c092cdb97f89b2c08c7b8e93f09e5587b7a210
AvalonEdit <144958945+AvalonEdit@users.noreply.github.com>
2023-12-06 13:04:51 -0600

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Spectre_Task_Manger
@@ -8,33 +9,44 @@ namespace Spectre_Task_Manger
         public MainWindow()
         {
             InitializeComponent();
+            PopulateProcesses();
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        private void PopulateProcesses()
         {
-            DragMove();
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void Maximize_Click(object sender, RoutedEventArgs e)
-        {
-            if (WindowState != WindowState.Maximized) 
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes) 
             { 
-                WindowState = WindowState.Maximized;
+                ProcessBox.Items.Add(process.ProcessName);
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) { DragMove(); }
+
+        private void Close_Click(object sender, RoutedEventArgs e) { this.Close(); }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e) { if (WindowState != WindowState.Maximized) { WindowState = WindowState.Maximized; } else { WindowState = WindowState.Normal; } }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) { WindowState = WindowState.Minimized; }
+
+        private void KillProcess_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedProcessName = ProcessBox.SelectedItem as string;
+            if (! string.IsNullOrEmpty(selectedProcessName)) 
+            {
+                Process[] processes = Process.GetProcessesByName(selectedProcessName);
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+                MessageBox.Show($"{selectedProcessName} killed successfully");
+
+                ProcessBox.Items.Remove(selectedProcessName);
             }
             else
             {
-                WindowState = WindowState.Normal;
+                MessageBox.Show("Please select a process to kill");
             }
-        }
-
-        private void Minimize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
         }
     }
 }
